9e5c617bf1fa1abab9da11f93e10a13ea1b11c9c
fuzedbook <ofierros123@gmail.com>
2026-01-20 19:26:28 -0600

    Día 1 y 2 – fundamentos de Python

---
+++ Semana_1/dia1_basico.py
@@ -0,0 +1,19 @@
+print("Hola mundo")
+
+#Definiendo una variable con camelCase
+nombreCompletoDeYo = "Oscar"
+
+#Definiendo una variable con Snake_case
+nombre_completo_de_yo = "Oscar"
+
+#Concatenar con +
+bienvenida = "Hola" + nombreCompletoDeYo + "Como estas"
+
+#Concatenar con f-string
+bienvenida = f"Hola {nombre_completo_de_yo} como estas?"
+
+#Operadores de pertenencia
+print ("Hola" in bienvenida) #True "hola" si esta en bievenida
+print("Hola" not in bienvenida)#False "Hola" si esta en bienvenida y estoy poniendo que si no esta.
+
+print(bienvenida)
